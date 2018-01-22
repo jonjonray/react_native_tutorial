b04d391c0a9b8230d0344791ec1401c61773bb10
jonjonray <jonjonray@gmail.com>
2018-01-22 10:28:05 -0800

    initial commit

---
+++ albums/src/components/header.js
@@ -0,0 +1,13 @@
+// Import libraries for making a Component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make a Component
+const Header = () => (
+  <Text>Albums</Text>
+);
+
+
+// Make the component available to other parts of the app
+
+export default Header;
