77ff887800f6ff2563207809acb8de969cc5d2a5
jonjonray <jonjonray@gmail.com>
2018-01-22 16:37:56 -0800

    finish extra components

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,13 +1,29 @@
 // Import libraries for making a Component
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 // Make a Component
 const Header = () => (
-  <Text>Albums</Text>
+  <View style={styles.container} >
+    <Text>Albums</Text>
+  </View>
 );
 
 
 // Make the component available to other parts of the app
+
+const styles = StyleSheet.create({
+  container: {
+    height: 60,
+    paddingTop: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2
+  }
+});
+
 
 export default Header;
