233e4571c6176de4be5b9a6bd884fff4fb62cfee
jonjonray <jonjonray@gmail.com>
2018-01-25 00:04:28 -0800

    continue work on auth

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
-import { View, TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
-
+import { Button, Card, CardSection, Input } from './common';
+import firebase from 'firebase';
 
 class LoginForm extends Component {
   constructor(props){
     super(props);
-    this.state = {state: ""};
+    this.state = {email: "", password: ""};
+  }
+
+  onButtonPress(){
+    const { email, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword(email, password);
   }
 
   render() {
     return (
       <Card>
         <CardSection>
-          <TextInput
-            value={this.state.text}
-            onChangeText={text => this.setState( { text })}
-            style={{height: 20, width: 100}} />
+          <Input
+            placeholder="user@gmail.com"
+            value={this.state.email}
+            label={"Email"}
+            onChangeText={email => this.setState( { email })}
+             />
         </CardSection>
 
-        <CardSection />
+        <CardSection>
+          <Input
+            secureTextEntry={true}
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+            />
+        </CardSection>
 
         <CardSection>
-          <Button>
+          <Button
+            onPress={this.onButtonPress.bind(this)}>
             Log In
           </Button>
         </CardSection>
